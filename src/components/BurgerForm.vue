8933772ef2a6a077d3a5c7d0ed4645e4635ccf9e
andré campelo <decocampeloreis@gmail.com>
2022-12-30 18:01:43 -0300

    add form

--- src/components/BurgerForm.vue
+++ src/components/BurgerForm.vue
@@ -14,7 +14,9 @@
             <label for="pao">Escolha seu pão</label>
             <select name="pao" id="pao" v-model="pao">
                 <option value="">Selecione o seu pão</option>
-                <option value="integral">Integral</option>
+                <option v-for="pao in paes" :key="pao.id" :value="pao.tipo">
+                    {{pao.tipo}}
+                </option>
             </select>
          </div>
 
@@ -22,15 +24,17 @@
             <label for="carne">Escolha a carne do seu burger:</label>
             <select name="carne" id="carne" v-model="carne">
                 <option value="">Selecione o tipo de carne</option>
-                <option value="maminha">maminha</option>
+                <option v-for="carne in carnes" :key="carne.id" :value="carne.tipo">
+                    {{carne.tipo}}
+                </option>
             </select>
          </div>
 
          <div id="opcionais-container" class="input-container">
             <label id="opcionais-title" for="opcionais">Selecione os opcionais:</label>
-            <div class="checkbox-container">
-                <input type="checkbox" name="opcionais" v-model="opcionais" value="salame">
-                <span>Salame</span>
+            <div class="checkbox-container" v-for="opcional in opcionaisdata" :key="opcional.id">
+                <input type="checkbox" name="opcionais" v-model="opcionais" :value="opcional.tipo">
+                <span>{{opcional.tipo}}</span>
             </div>
          </div>
 
@@ -43,7 +47,35 @@
 
 <script>
 export default {
-   name: 'BurgerForm'
+   name: 'BurgerForm',
+   data(){
+    return{
+        paes: null,
+        carnes: null,
+        opcionaisdata: null,
+        nome: null,
+        pao: null,
+        carne: null,
+        opcionais: [],
+        status: 'Solicitado',
+        msg: null
+    }
+   },
+   methods: {
+       async getIngredientes() {
+
+         const req = await fetch ("http://localhost:3000/ingredientes");
+         const data = await req.json();
+         
+         this.paes = data.paes;
+         this.carnes = data.carnes;
+         this.opcionaisdata = data.opcionais;
+
+       }
+   },
+   mounted(){
+        this.getIngredientes()
+   }
 }
 </script>
 

96f7f598139bc0f3ebae63b9b3ef82c809d62168
andré campelo <decocampeloreis@gmail.com>
2022-12-30 17:10:46 -0300

    add banner

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,45 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+   <NavBar :logo="logo_src" :alt="app_name" />
   <router-view/>
+  <Footer/>
 </template>
 
-<style>
+<script>
+import NavBar from './components/NavBar.vue'
+import Footer from './components/Footer.vue'
 
+export default {
+  components: {
+    NavBar,
+    Footer
+  },
+  data(){
+    return{
+      logo_src: '/img/logo.png',
+      app_name: 'Make Your Burger'
+    }
+  }
+}
+</script>
+
+
+<style>
+*{
+  font-family: Helvetica;
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+
+.main-container{
+  margin: 50px;
+  min-height: 250px;
+}
+
+h1{
+  text-align: center;
+  font-size: 42px;
+  margin-bottom: 30px;
+  color: #222;
+}
 </style>

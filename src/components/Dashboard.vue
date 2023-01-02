606486717a796042a0b8a76f597a9f8d77352b20
andré campelo <decocampeloreis@gmail.com>
2023-01-02 08:27:47 -0300

    att bug

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -26,7 +26,6 @@
            </div>
            <div>
             <select name="status" class="status" @change="updateBurger($event, burger.id)">
-                <option value="">Selecione</option>
                 <option v-for="s in status" :key="s.id" :value="s.tipo" :selected="burger.status == s.tipo">
                     {{s.tipo}}
                 </option>
